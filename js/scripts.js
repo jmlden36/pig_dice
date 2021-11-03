d1bd70202c0492d819a724d6ddaeaa05f98eef74
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-03 15:14:34 -0800

    finish day 1
    Co-authored-by: Ryan Ashby <rjapsu160@gmail.com>
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -51,45 +51,48 @@ let p2Score = new Score();
 let die = new Die();
 
 
-function updatePlayerScore(bool){
+function updatePlayerScore(){
+  if (die.p1turn) {
+    $("#player1Score").text = p1Score.totalScore;
+  } else{
+    $("#player2Score").text = p1Score.totalScore;
+  }
+}
 
+
+function updatePlayerTurn(){
+  if (die.p1turn) {
+    $("#playerTwoTurn").hide();
+    $("#playerOneTurn").show();
+  } else {
+    $("#playerOneTurn").hide();
+    $("#playerTwoTurn").show();
+  }
+}
+function updateTurnTotals(){
+  if (die.p1turn) {
+    // Look at displayContactDetails
+
+    // clear player1 turn totals
+    // loop through player1s turn totals and add all to #player1TurnTotals
+    //$("#player1TurnTotals").text = p1Score.currentScore
+  } else {
+    // clear player1 turn totals
+    // loop through player2s turn totals and add all to #player1TurnTotals
+    //$("#player2TurnTotals").text = p2Score.currentScore
+  }
+}
+function updateCurrentTurnRolls(){
+  // #player1 or #player2 turn score will continue to add rolled numbers as either player continues to push "Roll" until "Hold" is pushed or a "1" is rolled 
+  // #currentTurnRolls
+}
+function updateCurrentRollImage(currentRoll){
+  // based on current roll, toggle images to display correct number **use switch statement**
+  //toggle all images off, toggle on based on switch statement
 }
 /*
-BOWLING STYLE ANIMATION THINGY 
-switch to fail image
-setTimeout(switch back to die image, timeToDelayInMs)
-
-
-Row 1
-  Card 1 - Player 1 Total Score 
-  Card 2 - Whose Turn It Is
-  Card 3 - Player 2 Total Score
-Row 2
-  Card 1 - Player 1 Score for all previous turns  
-  Card 2 - Image of die =card-img-top/Current turn total = card-title/ All Rolls = currentTurnRolls/  
-  Card 3 - Player 2 Score for all previous turns
-  
-#player1Score or #player2Score score will update when the player clicks hold
-
-#player1TurnTotals or #player2TurnTotals will append turnTotals to to the ol
-when player clicks roll #currentTurnTotals use currentRoll to append to the ol
-
-#player1 or #player2 turn score will continue to add rolled numbers as either player continues to push "Roll" until "Hold" is pushed or a "1" is rolled 
-
-image of die with corresponding number should to appear to display the current roll's number 
-  pseudo code
-    If current role at index in array 
-    === 1, show <img roll1>
-    === 2, show <img roll2>
-    === 3, show <img roll3>
-    etc.
-
-show which player's turn it is in Row 1, Card 2
-
-
-show a winner when either player reaches a total score of 100
-
-
+Add functionality for game ending, and game beginning
+Add functionality for the crap talking array
 */ 
 
 //Buttons
@@ -100,15 +103,18 @@ function attachContactListeners(){
       if(currentRoll + p1Score.totalScore >= 100){
         //wingamefunction p1 wins
       }
-    } else if(!die.p1Turn) {
+    } else {
       if(currentRoll + p2Score.totalScore >= 100){
         //wingamefunction p2 wins
       }
     } 
     if(currentRoll === 1){
+    
       die.clearAllRolls;
+      p2Score.turnTotals.push(0);
+      p2Score.addRollsToTotalScore();
+      p2Score.clearCurrentScore();
       switchTurns(die.p1Turn);
-      // update who's turn ui
     } else {
       die.rolls.push(currentRoll);
       if(die.p1Turn){
